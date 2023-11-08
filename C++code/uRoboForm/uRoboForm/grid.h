d1d20c267c306024c466f118c1ec3f4feec5bade
Vishu196 <118134618+Vishu196@users.noreply.github.com>
2023-11-08 13:01:29 +0100

    sample class written for grid, not yet checked if running correctly

--- C++code/uRoboForm/uRoboForm/grid.h
+++ C++code/uRoboForm/uRoboForm/grid.h
@@ -1,5 +1,28 @@
 #pragma once
-class grid
-{
+#include <vector>
+#include <iostream>
+#include <string>
+
+class Grid {
+public:
+    Grid(std::vector<int> image, std::string orientation, std::pair<int, int> coord, int max_pos) {
+        this->image = image;
+        this->px_num = image.size();
+        this->orientation = orientation;
+        this->im_loc = coord;
+        if (orientation == "hor") {
+            this->max_pos = std::make_pair(coord.first + max_pos, coord.second);
+        }
+        else {
+            this->max_pos = std::make_pair(coord.first, coord.second + max_pos);
+        }
+    }
+
+private:
+    std::vector<int> image;
+    int px_num;
+    std::string orientation;
+    std::pair<int, int> im_loc;
+    std::pair<int, int> max_pos;
 };
 
