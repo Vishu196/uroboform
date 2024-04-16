cbb049d99f072126915ff7109cc18d2695e24b4e
Vishu196 <118134618+Vishu196@users.noreply.github.com>
2024-04-16 15:45:20 +0200

    used cv::resize function instead of imgSlice fn, and other changes as mentioned earlier

--- C++code/uRoboForm/uRoboForm/raw_edges.cpp
+++ C++code/uRoboForm/uRoboForm/raw_edges.cpp
@@ -1,80 +1,28 @@
 #include "raw_edges.h"
 
+using namespace cv;
+using namespace std;
+
 Mat raw_edges::ImageSliceR(Mat image, int n)
 {
-	Mat imgCopy = Mat(image.rows / n, image.cols / n, CV_8U, (int)image.step / n);
-
-	uint8_t* myData = image.data;
-	uint8_t* myCopyData = imgCopy.data;
-	int width = image.cols;
-	int height = image.rows;
-	int _stride = (int)image.step / n;//in case cols != strides
-	for (int i = 0; i < height; i += n)
-	{
-		for (int j = 0; j < width; j += n)
-		{
-			int val = myData[i * image.step + j];
-			int idx = (i / n * _stride + j / n);
-			*(myCopyData + idx) = val;
-		}
-	}
-	return imgCopy;
+	Mat newImg;
+	resize(image, newImg, Size(), 1.0 / n, 1.0 / n, cv::INTER_NEAREST);
+	return newImg;
 }
 
-double raw_edges::Main_FreqR(const vector<double> &B0, int start, int stop)
+double raw_edges::Calc_main_d(const vector<double> &mean0)
 {
-	double f_g = 0.0;
-	const int size = stop - start;
-
-	vector<double> B(size);
-
-	vector<double> image_window(size);
-	
-	for (int(k) = 0; k < size; k++) 
-	{
-		int w = k + start;
-		B[k] = B0[w];
-	}
-	
-	double Mean = Evaluation::MeanR(B);
-
-	vector<double> B1(size);
-
-	for (int i = 0; i < size; i++)
-	{
-		double x = B[i] - Mean;
-		B1[i] = x;
-	}
-
-	vector<double> wFun = signal_evaluation::BlackmanWindowR(size);
-	
-	for (int i = 0; i < size; i++)
-	{
-		double iw = B1[i] * wFun[i];
-		image_window[i] = iw;
-	}
-
-	vector<double> y1 = signal_evaluation::FFTR(image_window);
-
-	double n_g = signal_evaluation::Spek_InterpolR(y1);
-	uint32_t size_B = size;
-	f_g = n_g / size_B;
-
-	return f_g;
-}
-
-double raw_edges::Calc_main_d(const vector<double> &mean0, int freq_range)
-{
+	int interval = 50;
 	double main_d;
-	const int n1 = (((int)mean0.size() - freq_range) / 50) + 1;
+	const int n1 = (((int)mean0.size() - freq_range) / interval) + 1;
 	
 	vector<double> t1(n1);
 	int j = 0;
 
-	for (int i = 0; i < (mean0.size() - freq_range); i += 50)
+	for (int i = 0; i < (mean0.size() - freq_range); i += interval)
 	{
-		double tmp = Main_FreqR(mean0, i, i + freq_range);
-		t1[j] = (1 / tmp);
+		struct MFreq m = signal_evaluation::Main_FreqR(mean0, i, i + freq_range);
+		t1[j] = (1 / m.f_g);
 		j++;
 	}
 
@@ -85,30 +33,29 @@ double raw_edges::Calc_main_d(const vector<double> &mean0, int freq_range)
 
 void raw_edges::DisplayResult(const stage12 &s12)
 {
-	fifo.push(s12);
-
 	cout << "main_d_0: " << s12.main_d_0 << endl;
 	cout << "main_d_1: " << s12.main_d_1 << endl;
 	cout << "Stage 1 complete." << endl;
 
 }
 
-void raw_edges::ExecuteR(Mat Image, int freq_range)
+void raw_edges::ExecuteR(Mat Image)
 {
 	stage12 s12;
 
 	Mat Image2 = ImageSliceR(Image, 2);
 	
 	s12.mean0 = Evaluation::Mean0R(Image2);
-	s12.main_d_0 = Calc_main_d(s12.mean0, freq_range);
+	s12.main_d_0 = Calc_main_d(s12.mean0);
 	
 	s12.mean1 = Evaluation::Mean1R(Image2);
-	s12.main_d_1 = Calc_main_d(s12.mean1, freq_range);
+	s12.main_d_1 = Calc_main_d(s12.mean1);
 
 	s12.th_edge = Evaluation::MeanR(s12.mean0);
 
 	s12.img = Image;
 	s12.img2 = Image2;
 
+	fifo.push(s12);
 	DisplayResult(s12);
 }
--- C++code/uRoboForm/uRoboForm/raw_edges.h
+++ C++code/uRoboForm/uRoboForm/raw_edges.h
@@ -2,13 +2,14 @@
 #include"Evaluation.h"
 #include "signal_evaluation.h"
 #include "cqueue.h"
+#include "constants.h"
 
 struct stage12
 {		
-	Mat img;
-	Mat img2;
-	vector<double> mean0;
-	vector<double> mean1;
+	cv::Mat img;
+	cv::Mat img2;
+	std::vector<double> mean0;
+	std::vector<double> mean1;
 	double main_d_0;
 	double main_d_1;
 	double th_edge;
@@ -19,19 +20,16 @@ struct stage12
 
 class raw_edges
 {
-	friend class grid_pos01;
-
 private:
 
-	Mat ImageSliceR (Mat ImageR, int n);
-	double Main_FreqR (const vector<double> &B0, int start, int stop);
-	double Calc_main_d(const vector<double> &mean0, int freq_range);
+	cv::Mat ImageSliceR (cv::Mat ImageR, int n);
+	double Calc_main_d(const std::vector<double> &mean0);
 	void DisplayResult(const stage12& s12);
 	cqueue<stage12> fifo;
 
 public:
 
-	void ExecuteR(Mat image, int freq_range);
+	void ExecuteR(cv::Mat image);
 	stage12 getNext() 
 	{
 		stage12 s12;
