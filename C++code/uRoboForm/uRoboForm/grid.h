41d6891d2e1b6a01b84afe0db4c1df29881fe671
Vishu196 <118134618+Vishu196@users.noreply.github.com>
2024-05-03 13:04:50 +0200

    added fn for px_num in grid class

--- C++code/uRoboForm/uRoboForm/grid.h
+++ C++code/uRoboForm/uRoboForm/grid.h
@@ -14,13 +14,15 @@ public:
     Grid(cv::Mat image, bool is_hor, std::vector<int> coord, std::vector<double> maxPos)
     {
         this->image = image;
-        this->px_num = image.rows * image.cols;
+        get_px_num(px_num);
         this->is_hor = is_hor;
         this->im_loc = coord;
         addmaxPos(maxPos);
     }
 
     Grid() = default;
+
+    void get_px_num(int &px_num) { px_num = image.rows * image.cols; }
 
     void addmaxPos(const std::vector<double>& maxPos)
     {
