7a79e881460c1bcc835d6aa20748793e2741b99b
Vishu196 <118134618+Vishu196@users.noreply.github.com>
2024-05-24 21:17:51 +0200

    chnaged use of r2r plan to r2c and c2r in rfft and irfft, so that no more rearranging of terms is required. was hoping for time improvement but as of now, time consumption increased to 14-15ms, will chec again in some time code running correctly for all imgs

--- C++code/uRoboForm/uRoboForm/signal_evaluation.h
+++ C++code/uRoboForm/uRoboForm/signal_evaluation.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include<complex>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846264338327
@@ -23,8 +24,8 @@ public:
 	double static BlackmanWindowR(int n,int pos);
 	std::vector<double> static FFTR(const std::vector<double>& image_window);
 	std::vector<double> static Bandfilter(const std::vector<double>& x, int start, size_t end);
-	std::vector<double> static RFFT(const std::vector<double>& x);
-	std::vector<double> static IRFFT(const std::vector<double>& x);
+	std::vector<std::complex<double>> static RFFT(const std::vector<double>& x);
+	std::vector<double> static IRFFT( std::vector<std::complex<double>>& x);
 	struct MFreq static Main_FreqR(const std::vector<double>& B0, int start, int stop);
 };
 
